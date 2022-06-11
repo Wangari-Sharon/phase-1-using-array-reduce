92bc9912819c12a4ec5c80ffb73b6d587e1b69fd
Wangari-Sharon <wangarik35@gmail.com>
2022-06-11 09:07:07 +0300

    commit tree 402655785ccc077b7c48084455c791305fb3bc37

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+function addBattery(accumulator, element) {
+    return (accumulator += element);
+  }
+  
+  const totalBatteries = batteryBatches.reduce(addBattery, 0);
+  console.log(totalBatteries);
